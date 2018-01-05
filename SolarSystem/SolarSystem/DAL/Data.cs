a89e6bcb76ec4ba079edb914c18ffc630bc28de4
David Willart <dava.willart@gmail.com>
2018-01-05 22:00:05 +0200

    много чего полезного сделано

--- SolarSystem/SolarSystem/DAL/Data.cs
+++ SolarSystem/SolarSystem/DAL/Data.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,21 @@ using System.Threading.Tasks;
 namespace SolarSystem.DAL
 {
     class Data
-    {
+    { 
+        public List <string> SSData { get; set; }
+        public Data(string directory)
+        {
+            using (FileStream filestream = File.OpenRead(directory))
+            {
+                using (StreamReader streamReader = new StreamReader(filestream))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        string line = streamReader.ReadLine();
+                        SSData.Add(line);
+                    }
+                }
+            }
+        }
     }
 }
--- SolarSystem/SolarSystem/Program.cs
+++ SolarSystem/SolarSystem/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SolarSystem.DAL;
 using SolarSystem.TestFolder;
 
 namespace SolarSystem
@@ -11,8 +13,9 @@ namespace SolarSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi!");
-            TestClass tt = new TestClass();
+            string directory = ConfigurationManager.AppSettings.Get("directory");
+            DAL.Data data = new Data(directory);
+
         }
     }
 }
